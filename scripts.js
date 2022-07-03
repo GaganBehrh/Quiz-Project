c44b598d0742a9e47fc9735eae7037ed0d273475
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-07-03 17:41:10 +0000

    Question added to JS file

---
+++ scripts.js
@@ -0,0 +1,45 @@
+const questions = [{
+    id: 0,
+    question: "What langauage you speak in Germany?",
+    answer: [
+        { text: "French", isCorrect: false },
+        { text: "German", isCorrect: true },
+        { text: "English", isCorrect: false }
+        
+    ]
+
+},
+{
+    id: 1,
+    question: "What langauage you speak in France?",
+    answer: [
+        { text: "Spanish", isCorrect: false, isSelected: false },
+        { text: "French", isCorrect: true },
+        { text: "German", isCorrect: false }
+       
+    ]
+
+},
+{
+    id: 2,
+    question: "What is the capital of Germany",
+    answer: [
+        { text: "Hannover", isCorrect: false },
+        { text: "Berlin", isCorrect: true },
+        { text: "Munich", isCorrect: true }
+       
+    ]
+
+},
+{
+    id:3,
+    question:"What is the capital of France",
+    anmswer:[
+        {text:"paris", isCorrect:true},
+        {text:"Nantes", isCorrect:false},
+        {text:"Bordeaux",isCorrect:false},
+
+    ]
+}
+
+]
