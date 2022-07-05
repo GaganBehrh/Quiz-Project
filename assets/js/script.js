b6a3e78b6762159918c1c5bb508a71fda1166e7d
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-07-05 06:54:31 +0000

    Next button working and answer values being displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ const questions = [{
     i: 3,
     question: "What is the capital of France?",
     answer: [
-        { text: "paris", isCorrect: true },
+        { text: "Paris", isCorrect: true },
         { text: "Nantes", isCorrect: false },
         { text: "Bordeaux", isCorrect: false },
 
@@ -49,9 +49,9 @@ function loopthrough(i) {
     const q = document.getElementById("q");
     q.innerText = questions[i].question;
 
-    const answer1 = document.getElementById("ans1").value;
-    const answer2 = document.getElementById("ans2").value;
-    const answer3 = document.getElementById("ans3").value;
+    const answer1 = document.getElementById("ans1");
+    const answer2 = document.getElementById("ans2");
+    const answer3 = document.getElementById("ans3");
    
 
     answer1.innerText = questions[i].answer[0].text;
