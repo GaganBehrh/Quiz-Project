56848d12dc6e9a9bc1d105d5efaa5e7b1e8864e7
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-07-06 15:53:45 +0000

    Questions from script.js  moved to separate file  question.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,5 @@
-const questions = [{
-    i: 0,
-    question: "What is the most populous and most visited city in Switzerland?",
-    answer: [
-        { text: "Geneva", isCorrect: false },
-        { text: "Zurich", isCorrect: true },
-        { text: "Bern", isCorrect: false }
-
-    ]
-
-},
-{
-    i: 1,
-    question: "What is the most populous and most visited city in France?",
-    answer: [
-        { text: "Nantes", isCorrect: false, isSelected: false },
-        { text: "Paris", isCorrect: true },
-        { text: "Lyon", isCorrect: false }
-
-    ]
-
-},
-{
-    i: 2,
-    question: "What is the most populous and most visited city in Germany?",
-    answer: [
-        { text: "Aach", isCorrect: false },
-        { text: "Hamburg", isCorrect: true },
-        { text: "Calw", isCorrect: false }
-
-    ]
-
-},
-{
-    i: 3,
-    question: "What is the most populous and most visited city in Austria?",
-    answer: [
-        { text: "Vienna", isCorrect: true },
-        { text: "Salzburg", isCorrect: false },
-        { text: "Graz", isCorrect: false },
-
-    ]
-}
-]
-
+import {questions} from './question.js';
+var totalScore=0;
 var begin = true;
 function loopthrough(i) {
 
@@ -95,8 +52,9 @@ function loopthrough(i) {
 
     submitButton[0].addEventListener("click", () => {
         if (selectedvalue === "true") {
-            result[0].innerHTML = "True";
             result[0].style.color = "lightgreen";
+            totalScore++;
+            result[0].innerHTML = "True"+totalScore;
         } else {
             result[0].innerHTML = "False";
             result[0].style.color = "red";
