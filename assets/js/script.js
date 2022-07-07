21d0dbb9b68f3ffdfea44788812e8e587dfb7345
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-07-07 13:08:22 +0000

    Error free console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 import {questions} from './question.js';
 var totalScore=0;
 var begin = true;
-function loopthrough(i) {
 
+
+function loopthrough(i) {
     const q = document.getElementById("q");
     q.innerText = questions[i].question;
-
     const answer1 = document.getElementById("ans1");
     const answer2 = document.getElementById("ans2");
     const answer3 = document.getElementById("ans3");
-   
 
     answer1.innerText = questions[i].answer[0].text;
     answer2.innerText = questions[i].answer[1].text;
     answer3.innerText = questions[i].answer[2].text;
+   
    
 
     answer1.value = questions[i].answer[0].isCorrect;
@@ -24,25 +24,24 @@ function loopthrough(i) {
     var selectedvalue = "";
 
     answer1.addEventListener("click", () => {
-        answer1.style.backgroundColor = "#FFF89A";
-        answer2.style.backgroundColor = "#F7E2E2";
-        answer3.style.backgroundColor = "#F7E2E2";
+        answer1.style.backgroundColor = "yellow";
+        answer2.style.backgroundColor = "transparent";
+        answer3.style.backgroundColor = "transparent";
         selectedvalue = answer1.value;
     })
 
     answer2.addEventListener("click", () => {
-        
-        answer1.style.backgroundColor = "#F7E2E2";
-        answer2.style.backgroundColor = "#FFF89A";
-        answer3.style.backgroundColor = "#F7E2E2";
-        
+        answer1.style.backgroundColor = "transparent";
+        answer2.style.backgroundColor = "yellow";
+        answer3.style.backgroundColor = "transparent";
         selectedvalue = answer2.value;
+        
     })
 
-    answer3.addEventListener("click", () => {
-        answer1.style.backgroundColor = "#F7E2E2";
-        answer2.style.backgroundColor = "#F7E2E2";
-        answer3.style.backgroundColor = "#FFF89A";
+    answer3.addEventListener("click", () => {  
+        answer1.style.backgroundColor = "transparent";
+        answer2.style.backgroundColor = "transparent";
+        answer3.style.backgroundColor = "yellow";
         selectedvalue = answer3.value;
     })
 
@@ -54,13 +53,27 @@ function loopthrough(i) {
         if (selectedvalue === "true") {
             result[0].style.color = "lightgreen";
             totalScore++;
-            result[0].innerHTML = "True"+totalScore;
+            result[0].innerHTML = "True \n Your score is"+totalScore+"/4";
+            
         } else {
             result[0].innerHTML = "False";
             result[0].style.color = "red";
         }
     })
+    
+
 }
+
+let endGameButton = document.getElementById('endGameButton');
+endGameButton.addEventListener("click",endGame);   
+function endGame(){    
+    alert("Gams is over!!!"+totalScore);
+if(answer1.value=="true"  || answer2.value=="true"  || answer3.value=="true")
+totalScore++;
+console.log(totalScore);
+}
+
+
 
 if (begin) {
     loopthrough("0");
@@ -78,14 +91,6 @@ nextButton.addEventListener("click", () => {
     }
   
 })
-function endGame(){
-    
-alert("Gams is over!!!");
-}
-
-var score=0;
-let endGameButton = document.getElementById('endGameButton');
-endGameButton.addEventListener("click",endGame);
 
 
 
