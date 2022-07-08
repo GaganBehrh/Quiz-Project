9fdd3b528946053716cbc90b41bcc5b3cf3c08d1
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-07-08 11:36:25 +0000

    Question numbers added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 const questions = [{
     i: 0,
-    question: "What is the most populous and most visited city in Switzerland?",
+    question: "1/4. What is the most populous and most visited city in Switzerland?",
     answer: [
         { text: "Geneva", isCorrect: false },
         { text: "Zurich", isCorrect: true },
@@ -12,7 +12,7 @@ const questions = [{
 },
 {
     i: 1,
-    question: "What is the most populous and most visited city in France?",
+    question: "2/4. What is the most populous and most visited city in France?",
     answer: [
         { text: "Nantes", isCorrect: false, isSelected: false },
         { text: "Paris", isCorrect: true },
@@ -23,7 +23,7 @@ const questions = [{
 },
 {
     i: 2,
-    question: "What is the most populous and most visited city in Germany?",
+    question: "3/4. What is the most populous and most visited city in Germany?",
     answer: [
         { text: "Aach", isCorrect: false },
         { text: "Hamburg", isCorrect: true },
@@ -34,7 +34,7 @@ const questions = [{
 },
 {
     i: 3,
-    question: "What is the most populous and most visited city in Austria?",
+    question: "4/4. What is the most populous and most visited city in Austria?",
     answer: [
         { text: "Vienna", isCorrect: true },
         { text: "Salzburg", isCorrect: false },
@@ -57,8 +57,7 @@ var result = document.getElementsByClassName("result");
 result[0].innerText = "";
 var selectedvalue = "", currentSelectedValue = "";
 function calcScore(currentSelectedValue) {
-    //for (let b = 0; b < correctAnswers; b++) {
-      //  if (selectedvalue.toLowerCase() === correctAnswers(b).ans.toLowerCase()) {
+   
       if(currentSelectedValue==="true"){  
       score++;
             console.log("Score is "+score);
@@ -67,12 +66,7 @@ function calcScore(currentSelectedValue) {
         else if(currentSelectedValue==="false")
         result[0].innerText="Wrong, Score is "+score;
     }
-    /*event.preventDefault();
-    console.log(event.target.value);
-    if(event.target.value==="true")
-    score++;
-    else
-    score;*/
+   
 
 
 function loopthrough(i) {
@@ -95,22 +89,17 @@ function loopthrough(i) {
         answer1.style.backgroundColor = "yellow";
         answer2.style.backgroundColor = "transparent";
         answer3.style.backgroundColor = "transparent";
-        //console.log(answer1.value);
+    
         selectedvalue = answer1.value;
-        // console.log(selectedvalue);
+       
        if (answer1.value === "true") {
             result[0].style.color = "lightgreen";
-            result[0].innerHTML = "True";
-            // score++;
-            // console.log(answer1.value);
-          
+            result[0].innerHTML = "True";  
         }
         else {
             result[0].style.color = "red";
             result[0].innerHTML = "False";
         }
-
-
     })
 
     answer2.addEventListener("click", () => {
@@ -119,38 +108,24 @@ function loopthrough(i) {
         answer3.style.backgroundColor = "transparent";
         console.log(answer2.value);
         selectedvalue = answer2.value;
-        //console.log(selectedvalue);
         if (answer2.value === "true") {
             result[0].style.color = "lightgreen";
-            result[0].innerHTML = "True";
-            //score++;
-            //console.log(answer2.value);
-           
+            result[0].innerHTML = "True";  
         }
         else {
             result[0].style.color = "red";
-            result[0].innerHTML = "False";
-
-
+            result[0].innerHTML = "False"
         }
-
-
     })
 
     answer3.addEventListener("click", () => {
         answer1.style.backgroundColor = "transparent";
         answer2.style.backgroundColor = "transparent";
         answer3.style.backgroundColor = "yellow";
-        // console.log(answer3.value);
         selectedvalue = answer3.value;
-        // console.log(selectedvalue);
         if (answer3.value === "true") {
             result[0].style.color = "lightgreen";
-            result[0].innerHTML = "True";
-            //score++;
-            //  console.log(answer3.value);
-            
-
+            result[0].innerHTML = "True";  
         }
         else {
             result[0].style.color = "red";
@@ -159,17 +134,13 @@ function loopthrough(i) {
 
     })
 }
-
-    //var result = document.getElementsByClassName("result");
-    //result[0].innerText = "";
     const submitButton = document.getElementsByClassName("submitButton");
  
      submitButton[0].addEventListener("click", () => {
          if (selectedvalue === "true") {
              result[0].style.color = "lightgreen";
              console.log(calcScore(selectedvalue));
-             //totalScore++;
-             //result[0].innerHTML = "True \n Your score is"+totalScore+"/4";
+           
              
          } else {
              result[0].innerHTML = "False";
@@ -178,9 +149,8 @@ function loopthrough(i) {
          }
      })
 
-
-
-
+if(score>=4)
+alert("Game is over!!!");
 let endGameButton = document.getElementById('endGameButton');
 endGameButton.addEventListener("click", endGame);
 function endGame(event) {
@@ -194,16 +164,14 @@ function endGame(event) {
 if (begin) {
     loopthrough("0");
 }
-
 const nextButton = document.getElementsByClassName('nextButton')[0];
 var j = 0;
-
 nextButton.addEventListener("click", () => {
     begin = false;
     if (j < 3) {
         j++;
         loopthrough(j);
-       // console.log(j);
+
     }
 
 })
